85dda14386649c696daaad8b3ee3f5cbf6b15ac9
Medrick Zounlome <mzounlome@gmail.com>
2022-09-29 13:30:21 -0400

    button structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 
 import Button from "./components/Buttons/Button";
+import { MdOutlineLocalGroceryStore } from "react-icons/md";
 import SideNav from "./components/SideNav/SideNav";
 
 function App() {
@@ -9,6 +10,15 @@ function App() {
       <SideNav />
       <div>
         <Button />
+        <Button text="Blue Text" color="blue" />
+        <Button size="sm" text="small" />
+        <Button size="md" text="medium" />
+        <Button size="lg" text="large" />
+        <Button size="lg" text="outlined" variant="outlined" />
+        <Button startIcon="local_grocery_store" text="Icon" />
+        <Button endIcon="local_grocery_store" text="Icon" />
+        <Button text="Disabled" disabled />
+        <Button text="Hover" />
       </div>
     </div>
   );
--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,18 +1,50 @@
+import "./Button.scss";
+
+import { MdOutlineLocalGroceryStore } from "react-icons/md";
 import React from "react";
 import styled from "styled-components";
+
 const StyledButton = styled.button`
-  width: 81px;
-  height: 36px;
-  color: black;
+  width: ${({ size }) =>
+    size === "lg" ? "92px" : size === "md" ? "81px" : "73px"};
+  height: ${({ size }) =>
+    size === "lg" ? "42px" : size === "md" ? "36px" : "32px"};
   box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
   border-radius: 6px;
-  background: #e0e0e0;
-  color: #3f3f3f;
+  background: ${({ variant }) =>
+    variant === "outlined" ? "white" : "#E0E0E0"};
+  &:hover,
+  &:focus {
+    background: "pink";
+  }
+  border: ${({ variant }) =>
+    variant === "outlined" ? "1px solid #3D5AFE" : ""};
 `;
-const Button = ({ color, size, endIcon, variant, hover, focus }) => {
+
+const Button = ({
+  color,
+  size,
+  endIcon,
+  variant,
+  hover,
+  focus,
+  text,
+  startIcon,
+  disabled,
+}) => {
   return (
-    <div>
-      <StyledButton>Click Me</StyledButton>
+    <div className="button-container">
+      <StyledButton
+        color={color}
+        variant={variant}
+        size={size}
+        disabled={disabled}
+        hover={hover}
+      >
+        {startIcon === "local_grocery_store" && <MdOutlineLocalGroceryStore />}{" "}
+        {text ? text : "Default"}
+        {endIcon === "local_grocery_store" && <MdOutlineLocalGroceryStore />}
+      </StyledButton>
     </div>
   );
 };
